1e4fd1dd47c4368e0abc0406aa9c3e0663b758c2
Ewelka1299 <ewelinaboczar99@gmail.com>
2022-06-11 20:23:58 +0200

    plan adding started

--- src/repository/MilestoneRepository.php
+++ src/repository/MilestoneRepository.php
@@ -16,4 +16,23 @@ class MilestoneRepository extends Repository
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function addMilestone(Milestone $mil)
+    {
+        $stmt = $this->database->connect()->prepare('
+            INSERT INTO public.milestone (day_plan_id, milestone_type_id, location_name, street_name, street_number, milestone_start_time,milestone_end_time, milestone_description, image)
+            VALUES (?, 0, ?, ?, ?, ?, ?, ?, ?)
+        ');
+
+        $stmt->execute([
+            $mil->getDayPlanId(),
+            $mil->getLocationName(),
+            $mil->getStreetName(),
+            $mil->getStreetNumber(),
+            $mil->getMilestoneStartTime(),
+            $mil->getMilestoneEndTime(),
+            $mil->getMilestoneDescription(),
+            $mil->getImage()
+        ]);
+    }
 }
